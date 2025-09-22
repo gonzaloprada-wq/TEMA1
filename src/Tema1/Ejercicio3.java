dd444be3fb60d5d1b363d3a57d36b3381df1c396
gonzalo.prada <gonzalo.prada@106-22.info2>
2025-09-22 10:17:47 +0200

    22/09/2025

---
+++ src/Tema1/Ejercicio3.java
@@ -0,0 +1,32 @@
+package Tema1;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		//We create a new scanner.
+		Scanner reader = new Scanner(System.in);
+				
+		//We create a number variable called "añoActual"
+		int añoActual;
+		
+		//We create a number variable called "añoNacimiento"
+		int añoNacimiento;
+		
+		//We ask for "añoNacimiento"
+		System.out.print("¿En que año naciste?:");
+		añoNacimiento = reader.nextInt();
+		
+		//We ask for "añoActual"
+		System.out.print("Año actual:");
+		añoActual = reader.nextInt();
+		
+		//We are going to calculate the age of the reader:
+		System.out.print("Tienes " + ( añoActual - añoNacimiento ) + " años.");
+	
+		reader.close();
+	}
+
+}
