67079d55e9297387cd7f2d641b0407e2c2ef67d0
Gonzalo <Gonzalo@DESKTOP-76ILFO1>
2025-09-22 18:45:43 +0200

    22/09/2025 TEMA 1

---
+++ src/Tema1/Ejercicio5.java
@@ -0,0 +1,34 @@
+	package Tema1;
+
+	import java.util.Scanner;
+	
+	import java.util.Locale;
+
+	public class Ejercicio5 {
+
+		public static void main(String[] args) {
+			//We create a new Scanner called "reader"
+			Scanner reader = new Scanner(System.in);
+			
+			//We turn "reader" to a Local lenguage
+			reader.useLocale(Locale.US);
+			
+			//We create a new variable called "radio"
+			double radio;
+						
+				//We create a line asking for a number
+				System.out.print("Longitud del radio de la circunferencia en cm: ");
+				
+				//Now we waith for the answer
+				radio = reader.nextDouble();
+				
+				//Now we calculate with Math.PI
+				System.out.println("La longitud es de "+(2*Math.PI*radio)+" cm.");
+				System.out.print("El area mide "+(Math.PI*(radio*radio))+" cm2");
+			
+			
+			reader.close();
+	}
+		
+
+}
