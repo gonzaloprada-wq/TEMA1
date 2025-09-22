3a0f3bfd83814168aedc75413c5dbe79acd1b9ab
gonzalo.prada <gonzalo.prada@106-22.info2>
2025-09-22 10:05:54 +0200

    22/09/25

---
+++ src/Tema1/Ejercicio1.java
@@ -0,0 +1,25 @@
+package Tema1;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		//We create a new scanner.
+		Scanner reader = new Scanner(System.in);
+		
+		//We make a number variable with the name "numero"
+		double numero;
+		
+		//We print the message
+		System.out.print("Introduce numero:"); 
+		
+		//Here the console is waiting for a number
+		numero = reader.nextDouble();
+		
+		//Closing scanner
+		reader.close();
+		
+	}	
+}
---
+++ src/Tema1/Ejercicio2.java
@@ -0,0 +1,22 @@
+package Tema1;
+
+import java.util.Scanner; 
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		//We create a new scanner.
+		Scanner reader = new Scanner(System.in);
+		
+		//We create a number variable called "edad"
+		int edad;
+		
+		//Te console es waiting for a number
+		System.out.print("Introduce la edad que tendras o tienes este año:");
+		edad = reader.nextInt();
+		
+		System.out.print("El año que viene tendras " + (edad+1) + " años");
+		
+		//Closing scanner
+		reader.close();
+	}
+}
