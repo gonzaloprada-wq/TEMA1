ea2908787f9b6d08c18f8a052d5d04e71622973e
gonzalo.prada <gonzalo.prada@106-22.info2>
2025-09-23 12:52:50 +0200

    23/9/25

--- src/Tema1/Ejercicio7.java
+++ src/Tema1/Ejercicio7.java
@@ -9,31 +9,32 @@ public class Ejercicio7 {
 	public static void main(String[] args) {
 		
 		Scanner reader = new Scanner(System.in);
-		
+		//String name
 		String nombre;
-		
+		//String street
 		String calle;
-	
+		//String telephone
 		String telefono;
 		
-		
+		//Waiting for the name
 		System.out.print("Introduce un nombre:");
 		
 		nombre = reader.nextLine();
-		
+		//Waiting for the address
 		System.out.print("Introduce tu dirección:");
 		
 		calle = reader.nextLine();
-		
+		//Waiting for the phone
 		System.out.print("Introduce tu Telefono:");
 		
 		telefono = reader.nextLine();
-		
+		//Giving the name
 		System.out.println("Nombre: "+ nombre);
+		//Giving the street
 		System.out.println("Dirección: "+ calle);
+		//Giving the phone
 		System.out.println("Teléfono: "+ telefono);
-		
-	
+		//Closing the reader
 		reader.close();
 
 	}
