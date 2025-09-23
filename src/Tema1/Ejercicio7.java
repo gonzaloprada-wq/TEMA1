e37ae4449b57527f54ad3f78d0a81506a7887515
gonzalo.prada <gonzalo.prada@106-22.info2>
2025-09-23 13:33:03 +0200

    23/9/25 Ejercicio 8

--- src/Tema1/Ejercicio7.java
+++ src/Tema1/Ejercicio7.java
@@ -1,14 +1,13 @@
-package Tema1;
+		package Tema1;
 
-import java.util.Locale;
+		import java.util.Locale;
 
 import java.util.Scanner;
 
-public class Ejercicio7 {
-
-	public static void main(String[] args) {
+		public class Ejercicio7 {
+				public static void main(String[] args) {
 		
-		Scanner reader = new Scanner(System.in);
+						Scanner reader = new Scanner(System.in);
 		//String name
 		String nombre;
 		//String street
@@ -16,26 +15,27 @@ public class Ejercicio7 {
 		//String telephone
 		String telefono;
 		
-		//Waiting for the name
-		System.out.print("Introduce un nombre:");
+			//Waiting for the name
+			System.out.println("Introduce un nombre:");
 		
-		nombre = reader.nextLine();
-		//Waiting for the address
-		System.out.print("Introduce tu dirección:");
+			nombre = reader.nextLine();
+			//Waiting for the address
+			System.out.println("Introduce tu dirección:");
 		
-		calle = reader.nextLine();
-		//Waiting for the phone
-		System.out.print("Introduce tu Telefono:");
+			calle = reader.nextLine();
+			//Waiting for the phone
+			System.out.println("Introduce tu Telefono:");
 		
-		telefono = reader.nextLine();
-		//Giving the name
-		System.out.println("Nombre: "+ nombre);
-		//Giving the street
-		System.out.println("Dirección: "+ calle);
-		//Giving the phone
-		System.out.println("Teléfono: "+ telefono);
-		//Closing the reader
-		reader.close();
+			telefono = reader.nextLine();
+			//Giving the name
+			System.out.println("Nombre: "+ nombre);
+			//Giving the street
+			System.out.println("Dirección: "+ calle);
+			//Giving the phone
+			System.out.println("Teléfono: "+ telefono);
+			
+			//Closing the reader
+			reader.close();
 
 	}
 
---
+++ src/Tema1/Ejercicio8.java
@@ -0,0 +1,35 @@
+package Tema1;
+
+import java.util.Locale;
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+
+		public static void main(String[] args) {
+		//I make the scanner	
+		Scanner reader = new Scanner(System.in);
+		//I make a string for the name
+		String nombre;
+		//I make an int for the age
+		int edad;
+		
+		//I ask for the name
+		System.out.print("A continuación por tu nombre: ");
+	
+		//Now the reader is waiting for the name
+		nombre = reader.nextLine();
+	
+		//I ask for the age
+		System.out.print("A continuación por tu edad: ");
+		//Now the reader is waiting for the age
+		edad = reader.nextInt();
+	
+		//Now we give the result.
+		System.out.print("Hola "+ nombre + ", tienes "+ edad+ " años, ¡que mayor eres!");
+	
+	reader.close();
+	
+	}
+
+}
