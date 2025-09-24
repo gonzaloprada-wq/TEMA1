d0ef6d044de3f22b0459bb9affa0273ae1a5405c
gonzalo.prada <gonzalo.prada@106-22.info2>
2025-09-24 08:54:35 +0200

    24/9/25

--- src/Tema1/Ejercicio10.java
+++ src/Tema1/Ejercicio10.java
@@ -1,4 +1,4 @@
-	package Ejercicio10;
+	package Tema1;
 
 	import java.util.Scanner;
 
@@ -6,19 +6,21 @@
 
 	public class Ejercicio10 {
 		public static void main(String[] args) {
-		
+		//Here i made the scanner
 			Scanner reader = new Scanner(System.in);
-			
+		//Here i made the variable number
 			int numero;
 			
+			
+			
+		//Now Im waiting for the number
 			System.out.println("Porfavor, agrege su numero: ");
-			
+		//Now asking for the number
 			numero = reader.nextInt();
-			
+		//Here i made If and else
 			if(numero % 2 == 0) {System.out.println("El número " + numero + " es PAR");
-			
 			} else {System.out.println("El número " + numero + " es IMPAR");}
-			
+			//Here I close the Scanner
 		reader.close();
 	}
 
